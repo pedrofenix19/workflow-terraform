5c8b116f692d7205324a9485c685ca2241c2608e
Pedro Deniz <pedeniz@gmail.com>
2024-09-27 17:23:10 +0200

    initial commit

--- provider.tf
+++ provider.tf
@@ -8,6 +8,14 @@ terraform {
 }
 
 provider "aws" {
-  region = "eu-west-1"
+  region = "us-east-1"
 }
 
+terraform {
+  backend "s3" {
+    bucket = "curso-iac-avanzado-states"
+    key    = "pedro/modulo-github/terraform.tfstate"
+    region = "us-east-1"
+    dynamodb_table = "tabla-bloqueo-terraform2"
+  }
+}
